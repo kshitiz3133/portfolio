f5410d0b83a7505488047a88f2b563556aee5327
kshitiz <kshitizagarwal3133@gmail.com>
2024-02-25 12:33:14 +0530

    started

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:portfolio/Animation/wave.dart';
 
@@ -13,7 +15,7 @@ class _HomeState extends State<Home> {
   double topOne = 200;
   double topTwo = 450;
   double bot = 250;
-  double conw=50;//size
+  double conw = 50; //size
 
   @override
   Widget build(BuildContext context) {
@@ -25,7 +27,7 @@ class _HomeState extends State<Home> {
                 topOne = topOne - v.scrollDelta! / 4;
                 topTwo = topTwo - v.scrollDelta! / 2;
                 bot = bot - v.scrollDelta! / 5;
-                if(conw<110) {
+                if (conw < 110) {
                   conw = conw + v.scrollDelta! / 20;
                 }
               });
@@ -45,7 +47,7 @@ class _HomeState extends State<Home> {
                 ),
               ),
               Positioned(
-                left: ScreenUtil().setWidth(bot-200),
+                left: ScreenUtil().setWidth(bot - 200),
                 bottom: ScreenUtil().setHeight(380),
                 child: Text(
                   "Kshitiz Agarwal\'s\ ",
@@ -66,108 +68,185 @@ class _HomeState extends State<Home> {
                     color: Colors.transparent,
                   ),
                   Container(
-                      height: 1500.h,
-                      color: Colors.black,
-                      child: Stack(
-                        alignment: Alignment.center,
-                        children: [
-                          Positioned(
-                              top: ScreenUtil().setHeight(topOne+50),
-                              child: Text(
-                                "My projects",
-                                style: TextStyle(color: Colors.white,fontSize: 40.sp),
-                              )
-                          ),
-                          Positioned(
-                              top: ScreenUtil().setHeight(topTwo+300),
-                              child: Text(
-                                "(Created by me)",
-                                style: TextStyle(color: Colors.white.withOpacity(0.5)),
-                              )
-                          ),
-                          Positioned(
-                            top: ScreenUtil().setHeight(topTwo+500),
-                            child: SizedBox(
-                              width: MediaQuery.of(context).size.width,
-                              child: Padding(
-                                padding: const EdgeInsets.all(20.0),
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  children: [
-                                    Container(height: 5*conw.h,width: conw.w,color: Colors.white,child: Column(mainAxisAlignment: MainAxisAlignment.center,children: [
-                                      SizedBox(height: 100.h,),
-                                      Icon(Icons.chat_rounded,color: Colors.black,size: conw/2,),
-                                      SizedBox(height: conw.h,),
-                                      Text("Minimal Chatting App")
-                                    ],),),
-                                    Container(height: 5*conw.h,width: conw.w,color: Colors.white,child: Column(mainAxisAlignment: MainAxisAlignment.center,children: [
-                                      SizedBox(height: 100.h,),
-                                      Icon(Icons.chat_rounded,color: Colors.black),
-                                      SizedBox(height: conw.h,),
-                                      Text("Minimal Chatting App")
-                                    ],),),
-                                    Container(height: 5*conw.h,width: conw.w,color: Colors.white,child: Column(mainAxisAlignment: MainAxisAlignment.center,children: [
-                                      SizedBox(height: 100.h,),
-                                      Icon(Icons.chat_rounded,color: Colors.black,),
-                                      SizedBox(height: conw .h,),
-                                      Text("Minimal Chatting App")
-                                    ],),),
-                                  ],
-                                ),
+                    height: 1500.h,
+                    color: Colors.black,
+                    child: Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        Positioned(
+                            top: ScreenUtil().setHeight(topOne + 50),
+                            child: Text(
+                              "My projects",
+                              style: TextStyle(
+                                  color: Colors.white, fontSize: 40.sp),
+                            )),
+                        Positioned(
+                            top: ScreenUtil().setHeight(topTwo + 300),
+                            child: Text(
+                              "(Created by me)",
+                              style: TextStyle(
+                                  color: Colors.white.withOpacity(0.5)),
+                            )),
+                        Positioned(
+                          top: ScreenUtil().setHeight(topTwo + 500),
+                          child: SizedBox(
+                            width: MediaQuery.of(context).size.width,
+                            child: Padding(
+                              padding: const EdgeInsets.all(20.0),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Container(
+                                    height: 5 * conw.h,
+                                    width: conw.w,
+                                    color: Colors.white,
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        SizedBox(
+                                          height: 100.h,
+                                        ),
+                                        Icon(
+                                          Icons.chat_rounded,
+                                          color: Colors.black,
+                                          size: conw / 2,
+                                        ),
+                                        SizedBox(
+                                          height: conw.h,
+                                        ),
+                                        Text("Minimal Chatting App")
+                                      ],
+                                    ),
+                                  ),
+                                  Container(
+                                    height: 5 * conw.h,
+                                    width: conw.w,
+                                    color: Colors.white,
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        SizedBox(
+                                          height: 100.h,
+                                        ),
+                                        Icon(Icons.chat_rounded,
+                                            color: Colors.black),
+                                        SizedBox(
+                                          height: conw.h,
+                                        ),
+                                        Text("Minimal Chatting App")
+                                      ],
+                                    ),
+                                  ),
+                                  Container(
+                                    height: 5 * conw.h,
+                                    width: conw.w,
+                                    color: Colors.white,
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        SizedBox(
+                                          height: 100.h,
+                                        ),
+                                        Icon(
+                                          Icons.chat_rounded,
+                                          color: Colors.black,
+                                        ),
+                                        SizedBox(
+                                          height: conw.h,
+                                        ),
+                                        Text("Minimal Chatting App")
+                                      ],
+                                    ),
+                                  ),
+                                ],
                               ),
                             ),
                           ),
-                          Positioned(
-                              top: ScreenUtil().setHeight(topOne+1100),
-                              child: Text(
-                                "My Skills",
-                                style: TextStyle(color: Colors.white,fontSize: 30.sp),
-                              )
-                          ),
-                          Positioned(top: ScreenUtil().setHeight(topOne+1400),child: Transform.scale(
-                            scale: 2,
-                            child: Stack(
-                              children: [
-                                MyAnimation(),
-                                Positioned(left: 80,child: MyAnimation())
-                              ],
-                            ),
-                          ))
-                        ],
-                      ),
-                      /*Column(
+                        ),
+                        Positioned(
+                            top: ScreenUtil().setHeight(topOne + 1100),
+                            child: Text(
+                              "My Skills",
+                              style: TextStyle(
+                                  color: Colors.white, fontSize: 30.sp),
+                            )),
+                        Positioned(
+                            top: ScreenUtil().setHeight(topOne + 1250),
+                            child:
+                                Image(image: AssetImage('assets/skills.png'))),
+                        Positioned(
+                            top: ScreenUtil().setHeight(1400),
+                            child: Transform.scale(
+                              scale: 2,
+                              child: Stack(
+                                children: [
+                                  MyAnimation(),
+                                  Positioned(left: 80, child: MyAnimation())
+                                ],
+                              ),
+                            ))
+                      ],
+                    ),
+                    /*Column(
                   children: [
                     SizedBox(height: 50.h,),
                     Text("Test",textScaler: TextScaler.linear(0.8),textAlign: TextAlign.center,style: TextStyle(color: Colors.white)),
                   ],
                 ),*/
-                      ),
+                  ),
                   Container(
                     height: 1100.h,
                     color: Color(0xff4D70AD),
-                    child: Stack(children: [
-                      Positioned(
-                          left: ScreenUtil().setWidth(bot+200),
-                          top: ScreenUtil().setHeight(40),
-                          child: SizedBox(height: 400,child: Image(image: AssetImage('assets/fishes.png'),))
-                      ),
-                      Positioned(
-                          left: ScreenUtil().setWidth(bot+500),
-                          top: ScreenUtil().setHeight(100),
-                          child: SizedBox(height: 400,child: Image(image: AssetImage('assets/fishes.png'),))
-                      ),
-                      Positioned(
-                          left: ScreenUtil().setWidth(bot+50),
-                          top: ScreenUtil().setHeight(400),
-                          child: SizedBox(height: 400,child: Image(image: AssetImage('assets/fishes.png'),))
-                      ),
-                      Positioned(
-                          left: ScreenUtil().setWidth(bot-500),
-                          top: ScreenUtil().setHeight(20),
-                          child: SizedBox(height: 400,child: Image(image: AssetImage('assets/fishes.png'),))
-                      ),
-                    ],),
+                    child: Stack(
+                      children: [
+                        Positioned(
+                            top: ScreenUtil().setHeight(250),
+                            left: ScreenUtil().setWidth(150),
+                            child: Container(
+                              child: Text(
+                                "Flutter",
+                                style: TextStyle(fontSize: 30.sp),
+                              ),
+                            )),
+                        Positioned(
+                            left: ScreenUtil().setWidth(bot + 200),
+                            top: ScreenUtil().setHeight(40),
+                            child: SizedBox(
+                                height: 400,
+                                child: Image(
+                                  image: AssetImage('assets/fishes.png'),
+                                ))),
+                        Positioned(
+                            left: ScreenUtil().setWidth(bot + 500),
+                            top: ScreenUtil().setHeight(100),
+                            child: SizedBox(
+                                height: 400,
+                                child: Image(
+                                  image: AssetImage('assets/fishes.png'),
+                                ))),
+                        Positioned(
+                            left: ScreenUtil().setWidth(bot + 50),
+                            top: ScreenUtil().setHeight(400),
+                            child: SizedBox(
+                                height: 400,
+                                child: Image(
+                                  image: AssetImage('assets/fishes.png'),
+                                ))),
+                        Positioned(
+                            left: ScreenUtil().setWidth(bot - 500),
+                            top: ScreenUtil().setHeight(20),
+                            child: SizedBox(
+                                height: 400,
+                                child: Image(
+                                  image: AssetImage('assets/fishes.png'),
+                                ))),
+                      ],
+                    ),
                   ),
                 ],
               ),
